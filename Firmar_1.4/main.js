6d540ab9e0c6defc8d0bb09a78eeec3164732e53
Reyner <rlabradas@xetid.cu>
2024-09-16 09:25:36 -0400

    first commit

---
+++ Firmar_1.4/main.js
@@ -0,0 +1,264 @@
+(function () {
+    var index = "https://asistencia.xetid.cu/";
+    var signed, intervalstatus, fail, failcount;
+    var interval = 30000;
+    function init() {
+        chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+            switch (request.funct) {
+                case "myTogleFunc":
+                case "toglefunction":
+                    var status = togleInterval(request.status);
+                    sendResponse({status: status, message: request.status !== status ? 'Service togled.' : 'Fail.'});
+                    break;
+                case "startService":
+                    var status = togleInterval(true);
+                    sendResponse({status: status, message: status ? 'Service started.' : 'Fail.'});
+                    break;
+                case "stopService":
+                    var status = togleInterval(false);
+                    sendResponse({status: status, message: !status ? 'Service stoped.' : 'Fail.'});
+                    break;
+                case "clearLogs":
+                    sendResponse({fails: clearLogs()});
+                    break;
+                case "generateNextEnt":
+                    generateNextEnt();
+                    break;
+                case "generateNextSal":
+                    generateNextSal();
+                    break;
+                case "systemLog":
+                    systemLog(request.log);
+                    sendResponse({success: true});
+                    break;
+                default:
+                    sendResponse({error: !0, message: 'Undefined function.'});
+                    console.error({textStatus: 'Call to undefined function.', errorThrown: 204});
+                    break
+            }
+        });
+        if (window.localStorage.getItem("xetid-extension-sign-starttogle"))
+            togleInterval(true);
+    }
+    function systemLog(log) {
+        try {
+            var date = new Date();
+            var local = window.localStorage.getItem("xetid-extension-sign-log") ? JSON.parse(window.localStorage.getItem("xetid-extension-sign-log")) : [];
+            if (local.length > 50)
+                local = [];
+            local.splice(0, 0, {date: date.toLocaleString(), log: log});
+            window.localStorage.setItem("xetid-extension-sign-log", JSON.stringify(local));
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function clearLogs() {
+        try {
+            window.localStorage.removeItem("xetid-extension-sign-log");
+            failcount = null;
+            chrome.browserAction.setBadgeText({text: ''});
+            return failcount;
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function signInOut(cb) {
+        try {
+            $.ajax({type: "POST", url: index + 'firmar.php', success: function (data) {
+                    if (cb && typeof cb === 'function')
+                        cb(data);
+                    return data;
+                }, error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    cb(false, {XMLHttpRequest: XMLHttpRequest, textStatus: textStatus, errorThrown: errorThrown});
+                }});
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function logOut(cb) {
+        try {
+            $.ajax({type: "POST", url: index + 'logout.php', success: function (data) {
+                    if (cb && typeof cb === 'function')
+                        cb(data);
+                    return data;
+                }, error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    cb(false, {XMLHttpRequest: XMLHttpRequest, textStatus: textStatus, errorThrown: errorThrown});
+                }});
+        } catch (e) {
+            throw e;
+        }
+    }
+    function logIn(user, pass, cb) {
+        try {
+            $.ajax({type: "POST", url: index + 'app.php', data: {'l-form-username': user, 'l-form-password': pass}, success: function (data) {
+                    if (cb && typeof cb === 'function')
+                        cb(true, data);
+                    return data;
+                }, error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    cb(false, {XMLHttpRequest: XMLHttpRequest, textStatus: textStatus, errorThrown: errorThrown});
+                }});
+        } catch (e) {
+            throw e;
+        }
+    }
+    function getEntradaSalida() {
+        try {
+            var entrada = window.localStorage.getItem("xetid-extension-sign-begin-gen") ? window.localStorage.getItem("xetid-extension-sign-begin-gen") : window.localStorage.getItem("xetid-extension-sign-begin");
+            if (entrada) {
+                var ent = entrada.split(":");
+                entrada = new Date();
+                entrada.setHours(ent[0]);
+                entrada.setMinutes(ent[1]);
+            }
+            var salida = window.localStorage.getItem("xetid-extension-sign-end-gen") ? window.localStorage.getItem("xetid-extension-sign-end-gen") : window.localStorage.getItem("xetid-extension-sign-end");
+            if (salida) {
+                var sal = salida.split(":");
+                salida = new Date();
+                salida.setHours(sal[0]);
+                salida.setMinutes(sal[1]);
+            }
+            return{entrada: entrada, salida: salida};
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function generateNextEnt() {
+        if (window.localStorage.getItem("xetid-extension-sign-randomize-ent")) {
+            var next = generateNext(window.localStorage.getItem("xetid-extension-sign-randomize-ent"), window.localStorage.getItem("xetid-extension-sign-begin"));
+            window.localStorage.setItem("xetid-extension-sign-begin-gen", next.time);
+            systemLog("Siguiente entrada programada " + next.time);
+            return next.datetime;
+        } else
+            window.localStorage.removeItem("xetid-extension-sign-begin-gen");
+        return null;
+    }
+    function generateNextSal() {
+        if (window.localStorage.getItem("xetid-extension-sign-randomize-sal")) {
+            var next = generateNext(window.localStorage.getItem("xetid-extension-sign-randomize-sal"), window.localStorage.getItem("xetid-extension-sign-end"));
+            window.localStorage.setItem("xetid-extension-sign-end-gen", next.time);
+            systemLog("Siguiente salida programada " + next.time);
+            return next.datetime;
+        } else
+            window.localStorage.removeItem("xetid-extension-sign-end-gen");
+        return null;
+    }
+    function generateNext(randomize, hour) {
+        try {
+            if (randomize) {
+                var max = parseInt(randomize);
+                if (!isNaN(max)) {
+                    var randMin = Math.floor(Math.random() * max);
+                    var generatorDate = new Date();
+                    if (hour) {
+                        var initial = hour.split(":");
+                        generatorDate.setHours(initial[0]);
+                        generatorDate.setMinutes(Boolean(Math.floor(Math.random() * 2)) ? parseInt(initial[1]) + randMin : parseInt(initial[1]) - randMin);
+                    }
+                    return {
+                        datetime: generatorDate,
+                        time: generatorDate.getHours() + ":" + generatorDate.getMinutes()
+                    };
+                }
+            }
+            return;
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function isLaborable(fecha) {
+        try {
+            if (window.localStorage.getItem("xetid-extension-sign-daysofweek")) {
+                var wwd = JSON.parse(window.localStorage.getItem("xetid-extension-sign-daysofweek"));
+                return wwd.indexOf(fecha.getUTCDay().toString()) >= 0;
+            } else
+                return!1;
+        } catch (Exception) {
+            throw Exception;
+        }
+    }
+    function intervalEject() {
+        try {
+            if (window.localStorage.getItem("xetid-extension-sign")) {
+                var horaInicio = new Date();
+                if (isLaborable(horaInicio)) {
+                    var entradasalida = getEntradaSalida(), horaFin = new Date();
+                    horaFin.setMinutes(horaInicio.getMinutes() + 2);
+                    var entrada = (fail && fail.entrada) || entradasalida.entrada && entradasalida.entrada < horaFin && entradasalida.entrada > horaInicio;
+                    var salida = (fail && fail.salida) || entradasalida.salida && entradasalida.salida < horaFin && entradasalida.salida > horaInicio;
+                    if (!signed && (entrada || salida)) {
+                        var decript = Base64.decode(window.localStorage.getItem("xetid-extension-sign"));
+                        var decript = decript.split("<~>");
+                        logIn(decript[0], decript[1], function (success, logd) {
+                            if (success && logd) {
+                                var validation = {loguin: logd.search('href="logout.php"') > 0, entradafirmada: logd.search('<a class="btn btn-danger btn-lg disabled" href="firmar.php">Firmar Entrada</a>') > 0, salidafirmada: logd.search('<a class="btn btn-danger btn-lg disabled" href="firmar.php">Firmar Salida</a>') > 0};
+                                if (validation.loguin) {
+                                    systemLog("Logueando usuario.");
+                                    if ((entrada && !validation.entradafirmada) || (salida && !validation.salidafirmada)) {
+                                        signInOut(function () {
+                                            systemLog("Firmando " + (entrada ? "entrada." : "salida."));
+                                            logOut(function () {
+                                                if (entrada)
+                                                    signed = generateNextEnt();
+                                                else
+                                                    signed = generateNextSal();
+                                                signed = signed ? signed : new Date(), fail = null;
+                                                if (!failcount) {
+                                                    chrome.browserAction.setBadgeBackgroundColor({color: "#4285F4"});
+                                                    chrome.browserAction.setBadgeText({text: '*'});
+                                                }
+                                                systemLog("Deslogueando usuario.");
+                                            });
+                                        });
+                                    } else {
+                                        systemLog("Ya se ha firmado.");
+                                        logOut(function () {
+                                            signed = new Date(), fail = null;
+                                            systemLog("Deslogueando usuario.");
+                                        });
+                                    }
+                                } else {
+                                    signed = new Date(), fail = null;
+                                    systemLog("Usuario o contraseña incorrectos.");
+                                    failcount = failcount ? failcount + 1 : 1;
+                                    chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+                                    chrome.browserAction.setBadgeText({text: String(failcount)});
+                                    return;
+                                }
+                            } else if (logd) {
+                                signed = null, fail = window.localStorage.getItem("xetid-extension-sign-itin") == 'true' ? {
+                                    entrada: entrada,
+                                    salida: salida
+                                } : null;
+                                failcount = failcount ? failcount + 1 : 1;
+                                chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+                                chrome.browserAction.setBadgeText({text: String(failcount)});
+                                systemLog('Error al conectar.');
+                            }
+                        });
+                    }
+                    if (!entrada && !salida && signed && signed < horaInicio)
+                        signed = fail = null;
+                }
+            } else
+                throw{textStatus: 'Usuario o contraseña no definidos', errorThrown: 255};
+        } catch (e) {
+            throw e;
+        }
+    }
+    function togleInterval(start) {
+        try {
+            if (start && !intervalstatus) {
+                intervalstatus = setInterval(intervalEject, interval);
+                systemLog("Servicio iniciado.");
+            } else if (!start && intervalstatus) {
+                clearInterval(intervalstatus);
+                intervalstatus = signed = fail = null;
+                systemLog("Servicio detenido.");
+            }
+            return Boolean(intervalstatus);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    init();
+})();
---
+++ Firmar_1.6/popup.js
@@ -0,0 +1,211 @@
+$(function () {
+    var decript = null;
+    var init = function () {
+        if (window.localStorage.getItem("xetid-extension-sign")) {
+            decript = Base64.decode(window.localStorage.getItem("xetid-extension-sign"));
+            decript = decript.split("<~>");
+            if (decript[0])
+                $("input[name=l-form-username]").val(decript[0]);
+            if (decript[1])
+                $("input[name=l-form-password]").val(decript[1]);
+        }
+        if (window.localStorage.getItem("xetid-extension-sign-daysofweek")) {
+            var dofweek = JSON.parse(window.localStorage.getItem("xetid-extension-sign-daysofweek"));
+            $.each($("input[name='dow']"), function () {
+                $(this).prop('checked', dofweek.indexOf($(this).val().toString()) >= 0);
+            });
+        }
+        if (window.localStorage.getItem("xetid-extension-sign-itin"))
+            $("#sign-it").prop('checked', window.localStorage.getItem("xetid-extension-sign-itin") == 'true');
+        if (window.localStorage.getItem("xetid-extension-sign-begin"))
+            $("input[name=entrada]").val(window.localStorage.getItem("xetid-extension-sign-begin"));
+        if (window.localStorage.getItem("xetid-extension-sign-end"))
+            $("input[name=salida]").val(window.localStorage.getItem("xetid-extension-sign-end"));
+        if (window.localStorage.getItem("xetid-extension-sign-randomize-ent"))
+            $("input[name=randomize-ent]").val(window.localStorage.getItem("xetid-extension-sign-randomize-ent"));
+        if (window.localStorage.getItem("xetid-extension-sign-randomize-sal"))
+            $("input[name=randomize-sal]").val(window.localStorage.getItem("xetid-extension-sign-randomize-sal"));
+        if (window.localStorage.getItem("xetid-extension-sign-starttogle")) {
+            $("#toglestart").html("<i uk-icon=\"icon: stop\"></i>");
+            $("#togleclock").css({'color': '#666'});
+        } else {
+            $("#toglestart").html("<i uk-icon=\"icon: play\"></i>");
+            $("#togleclock").css({'color': 'rgb(93, 185, 98)'});
+        }
+        $("#form-sign").change(function () {
+            if (formisvalid() && formisditry())
+                $("#savechanges").removeAttr("disabled");
+            else
+                $("#savechanges").attr("disabled", true);
+        });
+        $("#savechanges").click(function () {
+            decript = [$("input[name=l-form-username]").val(), $("input[name=l-form-password]").val()];
+            var enccript = Base64.encode(decript[0] + "<~>" + decript[1]);
+            window.localStorage.setItem("xetid-extension-sign", enccript);
+            var wwd = [];
+            $.each($("input[name='dow']:checked"), function () {
+                wwd.push($(this).val());
+            });
+            window.localStorage.setItem("xetid-extension-sign-daysofweek", JSON.stringify(wwd));
+
+            var entrada = $("input[name=entrada]").val();
+            if (entrada !== window.localStorage.getItem("xetid-extension-sign-begin") || $("input[name=randomize-ent]").val() !== window.localStorage.getItem("xetid-extension-sign-randomize-ent")) {
+                window.localStorage.setItem("xetid-extension-sign-begin", entrada ? entrada : null);
+                var randomize = parseInt($("input[name=randomize-ent]").val());
+                if (isNaN(randomize) || randomize < 1)
+                    window.localStorage.removeItem("xetid-extension-sign-randomize-ent");
+                else if ($("input[name=randomize-ent]").val() !== window.localStorage.getItem("xetid-extension-sign-randomize-ent"))
+                    window.localStorage.setItem("xetid-extension-sign-randomize-ent", randomize);
+					chrome.runtime.sendMessage({funct: "generateNextEnt"}, function () {
+						loadLogs();
+                });
+            }
+            var salida = $("input[name=salida]").val();
+            if (salida !== window.localStorage.getItem("xetid-extension-sign-end") || $("input[name=randomize-sal]").val() !== window.localStorage.getItem("xetid-extension-sign-randomize-sal")) {
+                var randomize = parseInt($("input[name=randomize-sal]").val());
+                window.localStorage.setItem("xetid-extension-sign-end", salida ? salida : null);
+                if (isNaN(randomize) || randomize < 1)
+                    window.localStorage.removeItem("xetid-extension-sign-randomize-sal");
+                else if ($("input[name=randomize-sal]").val() !== window.localStorage.getItem("xetid-extension-sign-randomize-sal"))
+                    window.localStorage.setItem("xetid-extension-sign-randomize-sal", randomize);
+                chrome.runtime.sendMessage({funct: "generateNextSal"}, function () {
+					loadLogs();
+                });
+            }
+            $(this).attr("disabled", true);
+            systemLog("Cambios guardados.");
+        });
+        $("#toglestart").click(function () {
+            if (window.localStorage.getItem("xetid-extension-sign-starttogle"))
+                chrome.runtime.sendMessage({funct: "stopService"}, function (response) {
+					if (!response.status) {
+						window.localStorage.removeItem("xetid-extension-sign-starttogle");
+						$("#toglestart").html("<i uk-icon=\"icon: play\"></i>");
+						$("#togleclock").css({'color': 'rgb(93, 185, 98)'});
+					}
+					loadLogs();
+			
+                });
+            else if (window.localStorage.getItem("xetid-extension-sign") && (window.localStorage.getItem("xetid-extension-sign-begin") || window.localStorage.getItem("xetid-extension-sign-end"))) {
+                chrome.runtime.sendMessage({funct: "startService"}, function (response) {
+					if (response.status) {
+						window.localStorage.setItem("xetid-extension-sign-starttogle", !0);
+						$("#toglestart").html("<i uk-icon=\"icon: stop\"></i>");
+						$("#togleclock").css({'color': '#666'});
+					}
+					loadLogs();
+                });
+            }
+        });
+        $("#clearlogs").click(function () {
+            clearLogs();
+        });
+        $("#sign-it").change(function () {
+            window.localStorage.setItem("xetid-extension-sign-itin", $(this).is(':checked'));
+        });
+        loadLogs();
+    };
+    var systemLog = function (log) {
+        chrome.runtime.sendMessage({funct: "systemLog", log: log}, function (response) {
+			loadLogs();
+        });
+    };
+    var clearLogs = function () {
+        chrome.runtime.sendMessage({funct: "clearLogs"}, function (response) {
+			loadLogs();
+        });
+    };
+    var loadLogs = function () {
+        $("#logs").empty();
+        if (window.localStorage.getItem("xetid-extension-sign-log")) {
+            var logs = JSON.parse(window.localStorage.getItem("xetid-extension-sign-log"));
+            for (var i in logs)
+                $("#logs").append("<p class=\"p-log\"><i class=\"date-log\">" + logs[i].date + "</i> " + logs[i].log + "</p>");
+        }
+    };
+    var daysChanged = function () {
+        if (window.localStorage.getItem("xetid-extension-sign-daysofweek")) {
+            var dofweek = JSON.parse(window.localStorage.getItem("xetid-extension-sign-daysofweek"));
+            var res = false;
+            $.each($("input[name='dow']"), function () {
+                if ($(this).prop('checked')) {
+                    if (dofweek.indexOf($(this).val()) < 0) {
+                        res = true;
+                        return true;
+                    }
+                } else if (dofweek.indexOf($(this).val()) >= 0) {
+                    res = true;
+                    return true;
+                }
+            });
+            return res;
+        } else
+            return true;
+    };
+    var loguinchange = function () {
+        var user = $("input[name=l-form-username]").val();
+        var pass = $("input[name=l-form-password]").val();
+        return user && user.trim() && pass && pass.trim() && (decript && ((decript[0] && user !== decript[0]) || (decript[1] && pass !== decript[1])));
+    };
+    var formisditry = function () {
+        return(window.localStorage.getItem("xetid-extension-sign-begin") ? window.localStorage.getItem("xetid-extension-sign-begin") !== $("input[name=entrada]").val() : $("input[name=entrada]").val() !== '') || (window.localStorage.getItem("xetid-extension-sign-end") ? window.localStorage.getItem("xetid-extension-sign-end") !== $("input[name=salida]").val() : $("input[name=salida]").val() !== '') || daysChanged() || (window.localStorage.getItem("xetid-extension-sign-randomize-ent") ? window.localStorage.getItem("xetid-extension-sign-randomize-ent") !== $("input[name=randomize-ent]").val() : $("input[name=randomize-ent]").val() !== '') || (window.localStorage.getItem("xetid-extension-sign-randomize-sal") ? window.localStorage.getItem("xetid-extension-sign-randomize-sal") !== $("input[name=randomize-sal]").val() : $("input[name=randomize-sal]").val() !== '') || loguinchange();
+    };
+    var formisvalid = function () {
+        var valid = true;
+        var usuario = $("input[name=l-form-username]").val();
+        var password = $("input[name=l-form-password]").val();
+        if (usuario && usuario.trim())
+            $("input[name=l-form-username]").removeClass("uk-form-danger");
+        else {
+            $("input[name=l-form-username]").addClass("uk-form-danger");
+            $("input[name=l-form-username]").attr("placeholder", 'Campo obligatorio.');
+            valid = false;
+        }
+        if (password && password.trim())
+            $("input[name=l-form-password]").removeClass("uk-form-danger");
+        else {
+            $("input[name=l-form-password]").addClass("uk-form-danger");
+            $("input[name=l-form-password]").attr("placeholder", 'Campo obligatorio.');
+            valid = false;
+        }
+        var entrada = $("input[name=entrada]").val();
+        var salida = $("input[name=salida]").val();
+        if (entrada && entrada.trim() && salida && salida.trim()) {
+            var ent = entrada.split(":");
+            entrada = new Date();
+            entrada.setHours(ent[0]);
+            entrada.setMinutes(ent[1]);
+            var sal = salida.split(":");
+            salida = new Date();
+            salida.setHours(sal[0]);
+            salida.setMinutes(sal[1]);
+            if (entrada >= salida) {
+                $("input[name=entrada]").addClass("uk-form-danger");
+                $("input[name=salida]").addClass("uk-form-danger");
+                valid = false;
+            } else {
+                $("input[name=entrada]").removeClass("uk-form-danger");
+                $("input[name=salida]").removeClass("uk-form-danger");
+            }
+        } else {
+            $("input[name=entrada]").removeClass("uk-form-danger");
+            $("input[name=salida]").removeClass("uk-form-danger");
+        }
+        var randomize = parseInt($("input[name=randomize-ent]").val());
+        if (!isNaN(randomize) && (randomize < 0 || randomize > 30)) {
+            $("input[name=randomize-ent]").addClass("uk-form-danger");
+            valid = false;
+        } else {
+            $("input[name=randomize-ent]").removeClass("uk-form-danger");
+        }
+        randomize = parseInt($("input[name=randomize-sal]").val());
+        if (!isNaN(randomize) && (randomize < 0 || randomize > 30)) {
+            $("input[name=randomize-sal]").addClass("uk-form-danger");
+            valid = false;
+        } else {
+            $("input[name=randomize-sal]").removeClass("uk-form-danger");
+        }
+        return valid;
+    };
+    init();
+});
